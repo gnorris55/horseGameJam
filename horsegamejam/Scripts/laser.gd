de9fda915feb18fb6b1a6a086396a18cb13d3eba
The-Quinnzite-Mushroom <quinn@trusler.net>
2025-02-17 23:28:48 -0800

    lasers do damage

---
+++ horsegamejam/Scripts/laser.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var vel = Vector2(0,0)
+var length = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if we want base of laser to move
+	#position.x += delta*vel.x
+	#position.y += delta*vel.y
+	$Line2D.points[0] = get_parent().get_node("Horse").position#stay centered on player position (remove if shooting laser)
+	$Line2D.points[1] +=    length/$attack_timer.wait_time*vel*delta#laser reaches click by time up
+	#print($Line2D)
+	#$Area2D/CollisionShape2D.set_shape($Line2D)
+	#print()#.points[1])
+	$Area2D/CollisionShape2D.get_shape().a = $Line2D.points[0]
+	$Area2D/CollisionShape2D.get_shape().b = $Line2D.points[1]
+
+
+
+func _on_attack_timer_timeout() -> void:
+	queue_free()
+	
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("body entered by LASER")
+	if area.is_in_group("enemyArea2D"):
+		area.get_parent().take_damage(2)	
--- horsegamejam/Scripts/main_weapons.gd
+++ horsegamejam/Scripts/main_weapons.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 var attack_type = "laser"
 @onready var horse_node = get_parent().get_parent()
-
+var laser_scene = preload("res://Scenes/laser.tscn")
+@onready var main_scene = horse_node.get_parent()
 
 const LASER_ATTACK_TIME = 0.5
 
@@ -15,22 +16,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var vec = Vector2(get_global_mouse_position().x -horse_node.position.x,get_global_mouse_position().y -horse_node.position.y)
-	var angle = vec.angle()
 	if Input.is_action_just_pressed("attack"):
+		
 		if attack_type == "laser":
-			$attack_timer.wait_time = LASER_ATTACK_TIME
-			#$line2D.visible = true
-			
-			#print(str(get_parent().position.length()) + "   "+ str(vec.length()))
-			$Line2D.points = [Vector2(0,0),vec]
+			var l = laser_scene.instantiate()
+			main_scene.add_child(l)
 			
 			
-			$Line2D.visible = true
-	$Line2D.rotation = -angle
-			
-
-
-func _on_attack_timer_timeout() -> void:
-	$Line2D.visible = false
-	#pass # Replace with function body.
+			l.get_node("Line2D").points = [horse_node.position,horse_node.position]
+			l.get_node("attack_timer").wait_time = LASER_ATTACK_TIME
+			l.get_node("attack_timer").start()
+			var vec = Vector2(get_global_mouse_position().x - horse_node.position.x,get_global_mouse_position().y - horse_node.position.y)
+			l.vel = vec.limit_length(1)
+			l.length = vec.length()
+			print(l.get_node("attack_timer").wait_time)
