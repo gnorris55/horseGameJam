1e25f23d354719f7ed8a463b0589bc936cf494fb
The-Quinnzite-Mushroom <quinn@trusler.net>
2025-02-17 18:45:50 -0800

    laser improved

--- horsegamejam/Scripts/main_weapons.gd
+++ horsegamejam/Scripts/main_weapons.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 
 var attack_type = "laser"
+@onready var horse_node = get_node("/root/Horse")
+
+
+const LASER_ATTACK_TIME = 0.5
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,11 +15,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var vec = Vector2(get_global_mouse_position().x -horse_node.position.x,get_global_mouse_position().y -horse_node.position.y)
+	var angle = vec.angle()
 	if Input.is_action_just_pressed("attack"):
-		print("attacks")
 		if attack_type == "laser":
-			
+			$attack_timer.wait_time = LASER_ATTACK_TIME
 			#$line2D.visible = true
-			var vec = Vector2(get_global_mouse_position().x - get_parent().position.x,get_global_mouse_position().y - get_parent().position.y)
-			$Line2D.rotation = -get_node("/root/Horse").rotation 
-			$Line2D.points = [get_parent().position,get_global_mouse_position()]
+			
+			#print(str(get_parent().position.length()) + "   "+ str(vec.length()))
+			$Line2D.points = [Vector2(0,0),vec]
+			
+			
+			$Line2D.visible = true
+	$Line2D.rotation = -angle
+			
+
+
+func _on_attack_timer_timeout() -> void:
+	$Line2D.visible = false
+	#pass # Replace with function body.
