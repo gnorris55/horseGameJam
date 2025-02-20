e4bafba0df7637ad5f95e7c4869a6b6923654e9a
The-Quinnzite-Mushroom <quinn@trusler.net>
2025-02-20 14:33:21 -0800

    small change I don't remember what it was

--- horsegamejam/Scripts/movement.gd
+++ horsegamejam/Scripts/movement.gd
@@ -11,26 +11,26 @@ const BOUNDINGBOX = [-2251,-1500,2251,1500]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    if Input.is_action_pressed("left") and parent.position.x > BOUNDINGBOX[0]:
-        vel.x = -1
-    elif Input.is_action_pressed("right") and parent.position.x < BOUNDINGBOX[2]:
-        vel.x =1
-    else:
-        vel.x = 0
-    if Input.is_action_pressed("up") and parent.position.y > BOUNDINGBOX[1]:
-        vel.y = -1
-    elif Input.is_action_pressed("down") and parent.position.y < BOUNDINGBOX[3]:
-        vel.y = 1
-    else:
-        vel.y = 0
-        
-    vel.limit_length(1)
-    parent.position += vel*speed*delta
-    
-    var vec = Vector2(get_global_mouse_position().x - get_parent().position.x,get_global_mouse_position().y - get_parent().position.y)
-    parent.rotation = vec.angle()+ PI/2
+	if Input.is_action_pressed("left") and parent.position.x > BOUNDINGBOX[0]:
+		vel.x = -1
+	elif Input.is_action_pressed("right") and parent.position.x < BOUNDINGBOX[2]:
+		vel.x =1
+	else:
+		vel.x = 0
+	if Input.is_action_pressed("up") and parent.position.y > BOUNDINGBOX[1]:
+		vel.y = -1
+	elif Input.is_action_pressed("down") and parent.position.y < BOUNDINGBOX[3]:
+		vel.y = 1
+	else:
+		vel.y = 0
+		
+	vel.limit_length(1)
+	parent.position += vel*speed*delta
+	
+	var vec = Vector2(get_global_mouse_position().x - get_parent().position.x,get_global_mouse_position().y - get_parent().position.y)
+	parent.rotation = vec.angle()+ PI/2
