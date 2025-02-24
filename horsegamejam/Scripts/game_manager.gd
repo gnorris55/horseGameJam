f1c5829b407674dc73e6cb66cb964790d8aaf212
gnorris55 <graemenorris99@gmail.com>
2025-02-24 14:24:49 -0800

    fixed ending screen

--- horsegamejam/Scripts/game_manager.gd
+++ horsegamejam/Scripts/game_manager.gd
@@ -2,14 +2,16 @@ extends Node2D
 @onready var horse: Node2D = $Horse
 @onready var enemy_mananager: Node2D = $enemyMananager
 
-@onready var game_over_menu: Label = $CanvasLayer2/Control/Control/gameOverText
-@onready var restart_button: Button = $CanvasLayer2/Control/Control/restartButton
+@onready var game_over_menu: Label = $CanvasLayer/Control/Control/gameOverText
+@onready var restart_button: Button = $CanvasLayer/Control/Control/restartButton
+@onready var control: Control = $CanvasLayer/Control
 
 var game_state = 0
 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	control.hide()
 	game_over_menu.visible = false
 	restart_button.visible = false
 	$AudioStreamPlayer2D.play()
@@ -21,8 +23,10 @@ func _process(delta: float) -> void:
 	pass
 	
 func game_over():
+	control.show()
 	game_over_menu.visible = true
 	restart_button.visible = true
+	restart_button.z_index = 5
 	#enemy_mananager.queue_free()
 	horse.queue_free()
 
